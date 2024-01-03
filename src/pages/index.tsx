fb8dc89ab51b26de9380dde367d88a9da54018bb
NightTrek <Daniels@dual4t.com>
2024-01-03 00:45:56 -0800

    fix(hero): grammer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,9 +23,10 @@ const Hero = () => {
           <span className=" text-default-500">Web3</span> revolution.
         </div>
         <div className="px-2 text-base text-slate-400">
-          I traveled the world helping buisnesses implement innovative solutions
-          for their problems. After running a Web3 startup for 4 years I am
-          activly looking for new opportunities to build the future of Web3.
+          I have traveled the world, helping businesses implement innovative
+          solutions for their problems. After running a Web3 startup for 4
+          years, I am actively looking for new opportunities to build the future
+          of Web3.
         </div>
         <div className="my-8 flex px-2 text-lg">
           <ButtonBright title="Read about my work at Nugbase" link="/nugbase" />
